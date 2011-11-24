847eec8c982a0f10b97d715c71da42c46839d878
Florian Larysch <florolf@devbraindump.de>
2011-11-24 02:25:27 +0100

    return len in append_string

--- daemon/curl.c
+++ daemon/curl.c
@@ -34,6 +34,7 @@ static size_t append_string(const char *ptr, size_t size, size_t nmemb, void *us
 		return 0;
 
 	strncat(*str, ptr, in_len);
+	return in_len;
 }
 
 int get_key_curl(uint8_t uid[static 7], mf_key_t key_out) {
