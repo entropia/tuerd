a93231ad1ca06fbfa5992f70cbf7d9834be64cf9
Florian Larysch <florolf@devbraindump.de>
2011-11-24 03:04:05 +0100

    add some debugging

--- daemon/main.c
+++ daemon/main.c
@@ -7,8 +7,13 @@
 #include "curl.h"
 #include "util.h"
 
+int debug;
+
 int main(int argc, char **argv) {
 	log("tuerd starting up");
+
+	if(getenv("TUERD_DEBUG"))
+		debug = 1;
 
 	struct pcsc_context *pcsc_ctx = pcsc_init();
 	if(!pcsc_ctx) {
@@ -21,18 +26,26 @@ int main(int argc, char **argv) {
 
 		sleep(1);
 
+		debug("Waiting for card");
+
 		intf = pcsc_wait(pcsc_ctx);
 		if(!intf) {
 			log("pcsc_wait() failed");
 			continue;
 		}
 
+		debug("Successfully got card");
+
 		int auth_success;
 		uint8_t uid[7];
 
 		auth_success = desfire_authenticate(intf, get_key_curl, uid);
-		if(auth_success)
+		if(auth_success) {
+			debug("Auth succeeded, opening door");
 			open_door_curl(uid);
+		} else {
+			debug("Auth failed");
+		}
 
 		pcsc_close(pcsc_ctx, intf);
 	}
--- daemon/util.h
+++ daemon/util.h
@@ -1,1 +1,4 @@
-#define log(format, ...) fprintf(stderr, format "\n", ##__VA_ARGS__)
+extern int debug;
+
+#define log(format, ...) do { fprintf(stderr, format "\n", ##__VA_ARGS__); } while(0)
+#define debug(format, ...) if(debug) { log(format, ##__VA_ARGS__); }
