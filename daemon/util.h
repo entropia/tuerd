0cd0927381f575665fe06b09480980bea7ae3a52
Florian Larysch <florolf@devbraindump.de>
2011-12-03 00:55:51 +0100

    add die()

--- daemon/util.h
+++ daemon/util.h
@@ -1,4 +1,7 @@
+#include <stdlib.h>
+
 extern int debug;
 
 #define log(format, ...) do { fprintf(stderr, format "\n", ##__VA_ARGS__); } while(0)
 #define debug(format, ...) if(debug) { log(format, ##__VA_ARGS__); }
+#define die(format, ...) do { log(format, ##__VA_ARGS__); exit(EXIT_FAILURE); } while(0)
