3190da292accf855108de2be8300212d1cfad49c
Florian Larysch <florolf@devbraindump.de>
2012-02-12 19:26:11 +0100

    moved emergency open to main.c

--- daemon/main.c
+++ daemon/main.c
@@ -9,7 +9,9 @@
 #include "util.h"
 
 int debug;
-extern volatile sig_atomic_t open_requested;
+
+static volatile sig_atomic_t reader_crashed = 0, open_requested = 0;
+static int failcnt = 0;
 
 void check_config() {
 	if(!getenv("TUERD_GETKEY_URL"))
@@ -22,8 +24,24 @@ void check_config() {
 		die("Need TUERD_POLICY_AUTH");
 }
 
+void open_handler(int sig) {
+	if(reader_crashed)
+		open_requested = 1;
+}
+
+void register_handler() {
+	struct sigaction sa;
+	sa.sa_handler = open_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+
+	if(sigaction(SIGUSR1, &sa, NULL) < 0)
+		die("Registering signal handler failed");
+}
+
 int main(int argc, char **argv) {
 	check_config();
+	register_handler();
 
 	log("tuerd (rev " GIT_REV  ") starting up");
 
@@ -43,22 +61,36 @@ int main(int argc, char **argv) {
 
 		debug("Waiting for card");
 
-		intf = pcsc_wait(pcsc_ctx);
-		if(!intf) {
-			log("pcsc_wait() failed");
-			continue;
-		}
-
 		if(open_requested) {
-			uint8_t uid[7] = {0x23, 0x42, 0xD0, 0x05, 0xFA, 0x17, 0x00};
+			uint8_t uid[7] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 			open_requested = 0;
 
 			log("Manual open requested, opening door now");
 			open_door_curl(uid);
 		}
 
+		intf = pcsc_wait(pcsc_ctx);
+		if(!intf) {
+			log("pcsc_wait() failed");
+
+			if(++failcnt >= 3 && !reader_crashed) {
+				log("Failing too often, allowing for manual open");
+				reader_crashed = 1;
+			}
+
+			continue;
+		}
+
 		debug("Successfully got card");
 
+		// Getting card succeeded, reset crash state
+		if(reader_crashed)
+			log("Reader was broken recently. Disabling manual open now");
+
+		failcnt = 0;
+		reader_crashed = 0;
+
+		// Authenticate card
 		int auth_success;
 		uint8_t uid[7];
 
